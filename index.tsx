c6db08fa970e844dc52faf478fa3876ddb7357e5
delrotf <delrotf02@gmail.com>
2021-01-25 08:44:02 -0500

    1 file modified

--- index.tsx
+++ index.tsx
@@ -1,16 +1,10 @@
 import React from "react";
 import { render } from "react-dom";
-import { LoginPage } from "./pages";
 import { BrowserRouter } from "react-router-dom";
 import { Main } from "./Main";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import "./style.scss";
-
-interface AppProps {}
-interface AppState {
-  name: string;
-}
 
 const App = () => {
   return (
